0578980a697005f7863ae6723c2d75916a224022
Vikms95 <vms1995@hotmail.com>
2022-07-12 22:55:46 +0200

    Rewrite with Express

--- index.js
+++ index.js
@@ -1,53 +1,27 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 3000;
 
-http.createServer((req, res) =>{
-  let filePath = path.join(__dirname, 'public',
-  (req.url === '/' ? 'index.html' : req.url)
-  )
-  let contentType = 'text/html';
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
 
-  let extname = path.extname(filePath)
-  // Check ext and set content type
-  switch(extname){
-    case '.js':
-      contentType = 'text/javascript'
-      break
-    case '.css':
-      contentType = 'text/css'
-      break
-    case '.json':
-      contentType ='application/json'
-      break
-    case '.png':
-      contentType = 'image/png'
-      break
-    case '.jpg':
-      contentType = 'image/jpg'
-      break
-  }
-  
-  if(contentType === 'text/html' && extname == '') filePath += '.html'
-  // Other html files do not have an extname?
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/contact-me', (req, res) => {
+  res.render('contact-me')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/about/:id', (req, res) => {
+  res.render('about')
+})
 
 
-  fs.readFile(filePath, (err, content) => {
-    if(err){
-      if(err.code === 'ENOENT'){
-        fs.readFile(path.join(__dirname, 'public', '404.html'),
-        (err, content) => {
-          res.writeHead(200, {'Content-Type': 'text/html'})
-          res.end(content, 'utf-8')
-        })
-      }else{
-        res.writeHead(500)
-        res.end(`Server error: ${err.code}`)
-      }
-    }else{
-      res.writeHead(200, {'Content-Type': contentType})
-      res.end(content, 'utf-8')
-    }
-  })
 
-}).listen(8080)
+app.listen(PORT)
